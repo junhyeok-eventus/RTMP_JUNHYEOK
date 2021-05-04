e6e689d466c732c49588c54b5c71a9ab1dd6a81e
junhyeok <junhyeok@event-us.kr>
2021-05-04 11:30:01 +0900

    로그인/로그아웃 css 수정 logout 시 url 변경

--- RTMP_JUNHYEOK/Controllers/HomeController.cs
+++ RTMP_JUNHYEOK/Controllers/HomeController.cs
@@ -18,20 +18,6 @@ namespace RTMP_JUNHYEOK.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         public ActionResult Login(string msg)
         {
             return View();
@@ -57,7 +43,7 @@ namespace RTMP_JUNHYEOK.Controllers
         public ActionResult Logout(string name, string password)
         {
             Session["loginUser"] = null;
-            return View("Index");
+            return Redirect("~/User/Index");
         }
     }
 }
